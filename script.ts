70a9fc18ec37085b0657045743133c1fee94ef88
Timothy <128176649+Timothy-P@users.noreply.github.com>
2024-03-05 21:58:50 +0000

    The start of starts.

--- script.ts
+++ script.ts
@@ -1,1 +1,27 @@
+// Hey, thanks for going into here and checking it out. If you've been through my other projects, then you've noticed that I'm now using TS (typescript) and it's cool. I'll be using from now on. :)
 
+function Alpha() {
+    // Degree 1
+
+    /*let NI1 = document.getElementById("NI1").value
+    let DI1 = document.getElementById("DI1").value
+    let XI1 = document.getElementById("XI1").value
+    let BI1 = document.getElementById("BI1").value
+    let NI2 = document.getElementById("NI2").value
+    let DI2 = document.getElementById("DI2").value
+    let XI2 = document.getElementById("XI2").value
+
+    // Degree add/sub/mult
+    let DO = document.getElementById("DO").value
+
+    //Degree 2
+
+    let NI3 = document.getElementById("NI3").value
+    let DI3 = document.getElementById("DI3").value
+    let XI3 = document.getElementById("XI3").value
+    let BI2 = document.getElementById("BI2").value
+    let NI4 = document.getElementById("NI4").value
+    let DI4 = document.getElementById("DI4").value
+    let XI4 = document.getElementById("XI4").value*/
+    alert("This isn't currently finished.\nTry coming back later when I've got something done.")
+}
