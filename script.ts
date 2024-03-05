4da885e8e90135eb9ae4e4ef2a43a50945ca6961
Timothy <128176649+Timothy-P@users.noreply.github.com>
2024-03-05 23:23:58 +0000

    Working but it's difficult.

--- script.ts
+++ script.ts
@@ -1,27 +1,50 @@
 // Hey, thanks for going into here and checking it out. If you've been through my other projects, then you've noticed that I'm now using TS (typescript) and it's cool. I'll be using from now on. :)
 
 function Alpha() {
-    // Degree 1
+    // Degree one
+    //  First half
+    let NumInp1 = document.getElementById("NumInp1")
+    let XInp1 = document.getElementById("XInp1")
+    let ExpoInt1 = document.getElementById("ExpoInp1")
 
-    /*let NI1 = document.getElementById("NI1").value
-    let DI1 = document.getElementById("DI1").value
-    let XI1 = document.getElementById("XI1").value
-    let BI1 = document.getElementById("BI1").value
-    let NI2 = document.getElementById("NI2").value
-    let DI2 = document.getElementById("DI2").value
-    let XI2 = document.getElementById("XI2").value
+    //  Degree positive/negative
+    let NegInp1 = document.getElementById("NegInp1")
 
-    // Degree add/sub/mult
-    let DO = document.getElementById("DO").value
+    //  Second half
+    let NumInp2 = document.getElementById("NumInp2")
+    let XInp2 = document.getElementById("XInp2")
+    let ExpoInt2 = document.getElementById("ExpoInp2")
 
-    //Degree 2
 
-    let NI3 = document.getElementById("NI3").value
-    let DI3 = document.getElementById("DI3").value
-    let XI3 = document.getElementById("XI3").value
-    let BI2 = document.getElementById("BI2").value
-    let NI4 = document.getElementById("NI4").value
-    let DI4 = document.getElementById("DI4").value
-    let XI4 = document.getElementById("XI4").value*/
-    alert("This isn't currently finished.\nTry coming back later when I've got something done.")
+    // Degree operation
+    let DegInp = document.getElementById("OpInp")
+
+
+    // Degree two
+    //  First half
+    let NumInp3 = document.getElementById("NumInp3")
+    let XInp3 = document.getElementById("XInp3")
+    let ExpoInt3 = document.getElementById("ExpoInp3")
+
+    //  Degree two positive/negative
+    let NegInp2 = document.getElementById("NegInp2")
+
+    //  Second half
+    let NumInp4 = document.getElementById("NumInp4")
+    let XInp4 = document.getElementById("XInp4")
+    let ExpoInt4 = document.getElementById("ExpoInp4")
+
+    // Math time.
+    if (DegInp.value == "times") {
+        alert("DegInp == times")
+    }
+    else if (DegInp.value == "add") {
+        alert("DegInp == add")
+    }
+    else if (DegInp.value == "subtract") {
+        alert("DegInp == subtract")
+    }
+    else {
+        console.error("Invalid input for DegInp");
+    }
 }
